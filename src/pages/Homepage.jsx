bd9b5931d8788371896474f2664278525df7aee8
Hakan Bilgi <hknblg34@gmail.com>
2022-10-09 21:36:40 +0300

    Homepage.jsx new

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -3,7 +3,9 @@ import Picture from "../components/Picture";
 import data from "../helper/data";
 
 const Homepage = () => {
+
   return (
+    
     <div style={{ minHeight: "100vh" }}>
       <h1>Photo Album</h1>
       <div className="pictures">
@@ -12,7 +14,9 @@ const Homepage = () => {
         ))}
       </div>
     </div>
+
   );
 };
+
 
 export default Homepage;
