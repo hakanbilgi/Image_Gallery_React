cea2f743d00f364ce1c5a3ba098f49671d0af235
Hakan Bilgi <hknblg34@gmail.com>
2022-10-09 21:43:13 +0300

    finish

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import "./styles/style.css";
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 
 root.render(<App />);
 
